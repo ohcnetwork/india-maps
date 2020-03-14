4a726d8d400de63081a745f425c660d909198d0b
Gigin George <mail.gigin@gmail.com>
2020-03-15 02:19:51 +0530

    Dynamic Data; Title

--- src/App.js
+++ src/App.js
@@ -9,15 +9,6 @@ function App() {
   return (
     <div id="mapid" style={{height: '100vh'}}>
       <Map/>
-      <div style={{position:"absolute", background:"white", top:"15px", right:"15px", padding:"20px", zIndex:"100"}}>
-        <h4>
-        Local Patients: {indiaData.countryData.localTotal} <br/>
-        International Patients: {indiaData.countryData.intTotal} <br/>
-        Total Cases: {indiaData.countryData.total} <br/>
-        Total Cured/Discharged: {indiaData.countryData.cured_dischargedTotal} <br/>
-        Total Deaths: {indiaData.countryData.deathsTotal}
-        </h4>
-      </div>
     </div>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -10,38 +10,76 @@ import {
   TileLayer,
 } from 'react-leaflet';
 import geoLocation from './geoLocation.js';
-import indiaData from './indiaData.json';
 import logo from './logo.svg';
 import './App.css';
 
 const center = [22.9734, 78.6569]
 
 export default class MapContainer extends Component<{}> {
+  constructor(){
+    super()
+    this.state = {
+      indiaData: null
+    }
+  }
+  componentDidMount() {
+    // console.log("Fetching Data")
+    fetch("https://exec.clay.run/kunksed/mohfw-covid")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // console.log("Received Response")
+          this.setState({
+            indiaData: result
+          });
+        },
+        (error) => {
+          // console.log("Error Response")
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
     return (
-      <Map center={center} zoom={5}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {geoLocation.map(location => {
-          console.log(location.state + "|" + JSON.stringify(indiaData.stateData[location.state]))
-          const locationData = indiaData.stateData[location.state];
-          if(locationData.cases === 0)
-           return null;
-          return(
-          <Circle key={location.state} center={[location.latitude, location.longitude]} fillColor="blue" radius={8000*locationData.cases}>
-            <Popup>
-            <h3>{location.state}</h3><br/>
-            Cases: {locationData.cases},<br/>
-            Cured/Discharged: {locationData.cured_discharged},<br/>
-            Deaths: {locationData.deaths},<br/>
-            Helpline: {locationData.helpline}</Popup>
-          </Circle>)
-        }
+      <div>
+        <Map center={center} zoom={5}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {this.state.indiaData && geoLocation.map(location => {
+            console.log(location.state + "|" + JSON.stringify(this.state.indiaData.stateData[location.state]))
+            const locationData = this.state.indiaData.stateData[location.state];
+            if(locationData.cases === 0)
+             return null;
+            return(
+            <Circle key={location.state} center={[location.latitude, location.longitude]} fillColor="blue" radius={8000*locationData.cases}>
+              <Popup>
+              <h3>{location.state}</h3><br/>
+              Cases: {locationData.cases},<br/>
+              Cured/Discharged: {locationData.cured_discharged},<br/>
+              Deaths: {locationData.deaths},<br/>
+              Helpline: {locationData.helpline}</Popup>
+            </Circle>)
+          }
 
-        )}
-      </Map>
+          )}
+        </Map>
+        {this.state.indiaData &&
+        <div style={{position:"absolute", background:"white", top:"15px", right:"15px", padding:"20px", zIndex:"100"}}>
+          <h4>
+          Total Cases: {this.state.indiaData.countryData.total} <br/>
+          Local Patients: {this.state.indiaData.countryData.localTotal} <br/>
+          International Patients: {this.state.indiaData.countryData.intTotal} <br/>
+          Total Cured/Discharged: {this.state.indiaData.countryData.cured_dischargedTotal} <br/>
+          Total Deaths: {this.state.indiaData.countryData.deathsTotal}
+          </h4>
+        </div>}
+      </div>
     )
   }
 }
