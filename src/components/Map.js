2a628a6afe6f319b0677f08cad8ee3b25f43a571
Gigin George <mail.gigin@gmail.com>
2020-03-20 12:02:24 +0530

    Update Tile Layer

--- src/components/Map.js
+++ src/components/Map.js
@@ -112,7 +112,7 @@ export default function MapContainer() {
         <Map center={center} zoom={7}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
           />
         {
             indiaData && indiaData.stateData ? geoLocation.map(location => {
