2d869f989fbb8a87bf01a42eb19179d3ccd22333
sanoopbhaskerv <sanoopbhasker@gmail.com>
2020-03-21 05:38:51 -0500

    state-wise list added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import Map from './components/Map.js'
-
+import React from "react";
+import Map from "./components/Map.js";
+import IndiaData from "./components/stateWiseList/IndiaData";
+import SelectedLocationData from "./components/stateWiseList/SelectedLocationData";
+import classNames from "classnames/bind";
+const cx = classNames.bind(require("./App.module.css"));
 function App() {
+  const [indiaData, setIndiaData] = React.useState([]);
+  const [selectedLocationData, setSelectedLocationData] = React.useState(
+    (indiaData || {}).summary || {}
+  );
+  const [newsSearchKeyword, setNewsSearchKeyword] = React.useState("India");
+  const handleStateWiseDataSuccess = indiaData => {
+    setIndiaData(indiaData);
+    setSelectedLocationData(indiaData.summary);
+  };
+  const handleStateSelect = stateData => {
+    setNewsSearchKeyword(stateData.loc);
+    setSelectedLocationData(stateData);
+  };
   return (
-      <Map/>
+    <>
+      <section className={cx("app-wrapper")}>
+        <section className={cx("app-container")}>
+          <div className={cx("list-wrapper")}>
+            <h1>COVID-19 India Tracker</h1>
+            <IndiaData
+              indiaData={indiaData}
+              onStateSelect={handleStateSelect}
+            />
+          </div>
+          <div className={cx("new-wrapper")}>
+            <SelectedLocationData
+              locationData={{ ...selectedLocationData, loc: newsSearchKeyword }}
+            />
+          </div>
+          <div className={cx("map-wrapper")}>
+            <Map onStateWiseDataGetSuccess={handleStateWiseDataSuccess} />
+          </div>
+        </section>
+      </section>
+    </>
   );
 }
 
