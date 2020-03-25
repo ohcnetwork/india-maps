19f4930596dcda1182fa20067c9949819e2bd46c
Nikhil Chandrasenan Nair <nikhil.chandrasenan@litmus7.com>
2020-03-25 11:30:47 +0530

    Code refactored from functional components to class component

--- src/components/stateWiseList/SelectedLocationData.js
+++ src/components/stateWiseList/SelectedLocationData.js
@@ -1,48 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import NewsCard from "./NewsCard";
 import DetailedTile from "./DetailedTile";
 import DistrictWiseList from "./DistrictWiseList";
 import Chart from "../Chart";
 import classNames from "classnames/bind";
 const cx = classNames.bind(require("./stateWiseList.module.css"));
-export default function SelectedLocationData(props) {
-  const { locationData } = props;
-  const isDataLoaded =
-    locationData && Object.keys(locationData || {}).length > 0;
-  const [news, setNews] = React.useState([]);
-  React.useEffect(() => {
+export default class SelectedLocationData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      news: []
+    };
+  }
+
+  componentDidMount() {
     fetch(
-      `https://newsapi.org/v2/everything?q=coronavirus ${locationData.loc}&api=news&count=5&sortBy=publishedAt&apiKey=542dde4c96ed4664a7d652b9730c635c`
+      `https://newsapi.org/v2/everything?q=coronavirus ${this.props.locationData.loc}&api=news&count=5&sortBy=publishedAt&apiKey=542dde4c96ed4664a7d652b9730c635c`
     )
       .then(res => res.json())
       .then(
         result => {
-          setNews(result.articles || []);
+          this.setState({
+            news: result.articles || []
+          });
         },
-        error => {}
+        error => {
+          this.setState({
+            news: [],
+            error
+          });
+        }
       );
-  }, [locationData.loc]);
-  return (
-    <>
-      <section className={cx("list-wrapper")}>
-        {!isDataLoaded && <div>Loading...</div>}
-        {isDataLoaded && (
-          <section>
-            <h1>{locationData.loc}</h1>
-            <DetailedTile
-              locationData={{
-                ...locationData.summary,
-                total:
-                  locationData.summary.confirmedCasesIndian +
-                  locationData.summary.confirmedCasesForeign
-              }}
-            />
-            <Chart locationData={locationData} />
-            <DistrictWiseList districtWiseData={locationData.subLocations} />
-            <NewsCard news={news} />
+  }
+
+  render() {
+    const { locationData } = this.props;
+    const isDataLoaded =
+      locationData && Object.keys(locationData || {}).length > 0;
+    const { error, news } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else {
+      return (
+        <>
+          <section className={cx("list-wrapper")}>
+            {!isDataLoaded && <div>Loading...</div>}
+            {isDataLoaded && (
+              <section>
+                <h1>{locationData.loc}</h1>
+                <DetailedTile
+                  locationData={{
+                    ...locationData.summary,
+                    total:
+                      locationData.summary.confirmedCasesIndian +
+                      locationData.summary.confirmedCasesForeign
+                  }}
+                />
+                <Chart locationData={locationData} />
+                <DistrictWiseList
+                  districtWiseData={locationData.subLocations}
+                />
+                <NewsCard news={news} />
+              </section>
+            )}
           </section>
-        )}
-      </section>
-    </>
-  );
+        </>
+      );
+    }
+  }
 }
